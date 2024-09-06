5cc9446c6770e3b01403b652f7872e5d4e27b4ea
ArturNSilvaa <162454885+ArturNSilvaa@users.noreply.github.com>
2024-09-06 10:59:33 -0300

    Add files via upload

---
+++ aula02_ed_js/app.js
@@ -0,0 +1,104 @@
+// console.log("Olá mundo");
+
+// let nome = "Artuzãou BR"
+// let idade = 12
+// let altura = 1.73
+
+// console.log(nome, idade, altura, null, true, false)
+// nossa aula
+
+// let objeto = {
+    // "nome": "Artur Normandia",
+    // "idade": 19,
+    // "altura": 1.75
+// }
+
+// console.log(nome, idade, altura, null, true, false)
+// console.log(objeto)
+
+// let objeto2 ={
+    // nome,
+    // idade,
+    // altura
+// }
+// console.log(nome, idade, altura, null, true, false)
+// console.log(objeto)
+// console.log(objeto2)
+
+// Usando colchetes []
+
+// let modelos = ['Gol', 'Corsa', 'Fusca'];
+
+// Usando construtor Array
+// let marcas = Array('Chevrolet', 'Ford', 'Fiat');
+
+// Definindo um valor fixo atraves da Array
+// let acessorios = new Array(5);
+
+// criando um Vetor vazio
+// let cores = []
+
+// ou
+// let categoria = new Array();
+
+// console.log(modelos)
+// console.log(marcas)
+// console.log(acessorios)
+// console.log(cores)
+
+// EXIBINDO OU RECUPERANDO O ELEMENTO DO VETOR
+
+// console.log(modelos[0])
+// console.log(modelos[1])
+// console.log(modelos[2])
+
+// let nome_modelo = modelo[0];
+// console.log(nome_modelo)
+
+// adicionando o elemento no fim da array
+
+// modelos.push('HB20')
+
+// exibindo o vetor
+// console.log(modelos)
+
+// adicionando o elemento no inicio da array
+
+// modelos.unshift('Logan')
+// console.log(modelos)
+// adicionando o elemento no fim da array
+
+// modelos.shift()
+// console.log(modelos)
+
+// adicionando o elemento no fim da array
+
+// modelos.pop()
+// console.log(modelos)
+
+
+// FRUTAS
+// let frutas = ['Laranja', 'Melancia', 'Morango'];
+
+// let vegetais = ['Batata doce', 'Cenoura', 'Milho'];
+
+// let feira = frutas.concat(vegatais);
+
+// exibindo o vetor
+// console.log(feira);
+
+// selecionando e copiando array
+// let frutas_ = ['Laranja', 'Melancia', 'Morango']
+// let frutas_sel = frutas_.slice(1, 3)
+
+// console.log(frutas_sel)
+
+// alterando ou removendo elemento da array
+// frutas_.splice(1,1,'Manga')
+
+// console.log(frutas_)
+
+let frutas = ['laranja', 'melancia', 'morango','uva']
+let existe = frutas.includes('morango');
+
+console.log(existe)
